d3e13a77e7352e6abd4bff669aacd8c8f655610a
Alex <sampekmoy@gmail.com>
2023-11-24 14:21:35 +0400

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,11 +2,17 @@
 /// <reference types="pino-http" />
 import { type LoggerOptions } from 'pino';
 import { type Options } from "rotating-file-stream";
+interface GelfOptions {
+    host?: string;
+    port?: number;
+    protocol?: 'tcp' | 'http' | 'https' | 'udp';
+}
 export declare const createLogger: (options: LoggerOptions, rotateOptions: Partial<Options> & {
     destination: string;
-}) => {
+}, gelfOptions?: GelfOptions) => {
     logger: import("pino").Logger<LoggerOptions>;
     middlewareLogger: import("pino-http").HttpLogger<import("http").IncomingMessage, import("http").ServerResponse<import("http").IncomingMessage>, {
         logger: import("pino").Logger<LoggerOptions>;
     }>;
 };
+export {};
--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,8 @@ const pino_1 = __importDefault(require("pino"));
 const pino_http_1 = __importDefault(require("pino-http"));
 const pino_pretty_1 = __importDefault(require("pino-pretty"));
 const rotate_destination_1 = __importDefault(require("./rotate-destination"));
-const createLogger = (options, rotateOptions) => {
+const child_process_1 = require("child_process");
+const createLogger = (options, rotateOptions, gelfOptions) => {
     const streams = [
         {
             stream: (0, rotate_destination_1.default)(Object.assign({ interval: '1d', size: '10M', maxFiles: 10, maxSize: '20M' }, rotateOptions))
@@ -17,6 +18,22 @@ const createLogger = (options, rotateOptions) => {
             stream: (0, pino_pretty_1.default)(),
         },
     ];
+    if (gelfOptions) {
+        const args = ['log'];
+        if (gelfOptions.host) {
+            args.push('-h', gelfOptions.host);
+        }
+        if (gelfOptions.port) {
+            args.push('-p', gelfOptions.port.toString());
+        }
+        if (gelfOptions.protocol) {
+            args.push('-P', gelfOptions.protocol);
+        }
+        const child = (0, child_process_1.spawn)('pino-gelf', args);
+        streams.push({
+            stream: child.stdin
+        });
+    }
     const logger = (0, pino_1.default)(options, pino_1.default.multistream(streams));
     const middlewareLogger = (0, pino_http_1.default)({ logger });
     return { logger, middlewareLogger };
