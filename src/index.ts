512188b20d851971742d2ad98fa6f4e8cd1535e9
Alex <sampekmoy@gmail.com>
2023-11-24 14:21:24 +0400

    add pino gelf

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-import pino, { type LoggerOptions } from 'pino';
+import pino, {DestinationStream, Level, type LoggerOptions, StreamEntry} from 'pino';
 import pinoHttp from 'pino-http';
 import pretty from 'pino-pretty'
 import rotateDestination from "./rotate-destination";
 import { type Options } from "rotating-file-stream";
+import { spawn } from "child_process";
 
-export const createLogger = (options: LoggerOptions, rotateOptions: Partial<Options> & { destination: string }) => {
+interface GelfOptions {
+  host?: string;
+  port?: number;
+  protocol?: 'tcp' | 'http' | 'https' | 'udp'
+}
+
+export const createLogger = (
+  options: LoggerOptions,
+  rotateOptions: Partial<Options> & { destination: string },
+  gelfOptions?: GelfOptions
+) => {
   const streams = [
     {
       stream: rotateDestination({
@@ -18,7 +29,29 @@ export const createLogger = (options: LoggerOptions, rotateOptions: Partial<Opti
     {
       stream: pretty(),
     },
-  ];
+  ] as (DestinationStream | StreamEntry<Level>)[] ;
+
+  if (gelfOptions) {
+    const args = ['log']
+
+    if (gelfOptions.host) {
+      args.push('-h', gelfOptions.host)
+    }
+
+    if (gelfOptions.port) {
+      args.push('-p', gelfOptions.port.toString())
+    }
+
+    if (gelfOptions.protocol) {
+      args.push('-P', gelfOptions.protocol)
+    }
+
+    const child = spawn('pino-gelf', args);
+
+    streams.push({
+      stream: child.stdin
+    })
+  }
 
   const logger = pino(options, pino.multistream(streams));
 
